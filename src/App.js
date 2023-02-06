94b5595bf4eedfe06515a8a76defba2d3fc278c9
Juanpa8830 <jp3_4@hotmail.com>
2023-02-06 16:52:33 -0500

    Add Navbar with routes and links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { getApiRockets } from './redux/rockets/Rocket';
+import Rockets from './Pages/Rockets';
+import Missions from './Pages/Missions';
+import MyProfile from './Pages/MyProfile';
 import './App.css';
+import planet from './images/planet.png';
 
 function App() {
+  const dispatch = useDispatch();
+  dispatch(getApiRockets());
+
   return (
-    <div className="App">
-      
+
+    <div className="root">
+      <nav className="nav" id="flex">
+        <img src={planet} className="d-inline-block" width="60" height="60" alt="React Bootstrap logo" />
+        <h1 className="navtitle">Space Travelers Hub</h1>
+        <ul className="navlist" id="flex">
+          <li className="listItem"><Link className="listLink1" to="/">Rockets</Link></li>
+          <li className="listItem"><Link className="listLink2" to="/Pages/Missions">Missions</Link></li>
+          <li className="listItem"><Link className="listLink2" to="/Pages/MyProfile">My profile</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/Pages/Missions" element={<Missions />} />
+        <Route path="/Pages/Missions" element={<MyProfile />} />
+      </Routes>
     </div>
   );
 }
