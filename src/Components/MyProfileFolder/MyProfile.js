c4ce87938b233ed8c7f5499dd347a864031646c1
Juanpa8830 <jp3_4@hotmail.com>
2023-02-07 14:25:27 -0500

    Render reserved rockets in Myprofile page

---
+++ src/Components/MyProfileFolder/MyProfile.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useSelector } from 'react-redux'; // se usa para acceder al estado en el store
+import ProfileRocketItem from './ProfileRocketItem';
+
+const MyProfile = () => {
+  const rockets = useSelector((state) => state.Rockets);
+  return (
+    <div className="container">
+      <div className="profilediv" id="flex">
+        <div className="<joinedmissions">
+          <h1>My Missions</h1>
+        </div>
+        <div className="<resrockets">
+          <h1>My rockets</h1>
+
+          {rockets.filter((item) => item.reserved === true).length === 0 ? (
+            <h2>No rockets reserved</h2>)
+            : (
+              <ul className="rocketlist" id="flex">
+                {rockets.filter((item) => item.reserved === true).map((rocket) => {
+                  const {
+                    name, id,
+                  } = rocket;
+
+                  return (
+                    <ProfileRocketItem
+                      key={id}
+                      name={name}
+                    />
+                  );
+                })}
+              </ul>
+            )}
+
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default MyProfile;
