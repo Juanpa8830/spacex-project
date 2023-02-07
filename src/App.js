5ec135f11d92958ca78730d219d38056f6290281
Juanpa8830 <jp3_4@hotmail.com>
2023-02-06 21:02:31 -0500

    Add classes to jsx tags

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux';
 import { getApiRockets } from './redux/rockets/Rocket';
 import Rockets from './Pages/Rockets';
 import Missions from './Pages/Missions';
-import MyProfile from './Pages/MyProfile';
+import MyProfile from './Pages/MyProfilepage';
 import './App.css';
 import planet from './images/planet.png';
 
@@ -16,19 +16,22 @@ function App() {
 
     <div className="root">
       <nav className="nav" id="flex">
-        <img src={planet} className="d-inline-block" width="60" height="60" alt="React Bootstrap logo" />
-        <h1 className="navtitle">Space Travelers Hub</h1>
+        <div className="navleft" id="flex">
+          <img src={planet} className="planet" width="60" height="60" alt="React Bootstrap logo" />
+          <h1 className="navtitle">Space Travelers&apos; Hub</h1>
+        </div>
         <ul className="navlist" id="flex">
           <li className="listItem"><Link className="listLink1" to="/">Rockets</Link></li>
           <li className="listItem"><Link className="listLink2" to="/Pages/Missions">Missions</Link></li>
-          <li className="listItem"><Link className="listLink2" to="/Pages/MyProfile">My profile</Link></li>
+          <div className="vert-div" />
+          <li className="listItem"><Link className="listLink2" to="/Pages/MyProfilepage">My profile</Link></li>
         </ul>
       </nav>
 
       <Routes>
         <Route path="/" element={<Rockets />} />
         <Route path="/Pages/Missions" element={<Missions />} />
-        <Route path="/Pages/Missions" element={<MyProfile />} />
+        <Route path="/Pages/MyProfilepage" element={<MyProfile />} />
       </Routes>
     </div>
   );
