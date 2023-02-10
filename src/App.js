7a3734dfd51fc7ffd768347c0e257c01a5c67f3d
Juanpa8830 <jp3_4@hotmail.com>
2023-02-10 12:09:35 -0500

    Fix dispatch method to correponding component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,34 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
-import { getApiRockets } from './redux/rockets/Rocket';
+
 import Rockets from './Pages/Rockets';
 import Missions from './Pages/Missions';
 import MyProfile from './Pages/MyProfilepage';
 import './App.css';
 import planet from './images/planet.png';
-import store from './redux/configureStore';
 
-function App() {
-  useEffect(() => {
-    store.dispatch(getApiRockets());
-  }, []);
+const App = () => (
 
-  return (
+  <div className="root">
+    <nav className="nav" id="flex">
+      <div className="navleft" id="flex">
+        <img src={planet} className="planet" width="60" height="60" alt="React Bootstrap logo" />
+        <h1 className="navtitle">Space Travelers&apos; Hub</h1>
+      </div>
+      <ul className="navlist" id="flex">
+        <li className="listItem"><Link className="listLink1" to="/">Rockets</Link></li>
+        <li className="listItem"><Link className="listLink2" to="/Pages/Missions">Missions</Link></li>
+        <div className="vert-div" />
+        <li className="listItem"><Link className="listLink3" to="/Pages/MyProfilepage">My profile</Link></li>
+      </ul>
+    </nav>
 
-    <div className="root">
-      <nav className="nav" id="flex">
-        <div className="navleft" id="flex">
-          <img src={planet} className="planet" width="60" height="60" alt="React Bootstrap logo" />
-          <h1 className="navtitle">Space Travelers&apos; Hub</h1>
-        </div>
-        <ul className="navlist" id="flex">
-          <li className="listItem"><Link className="listLink1" to="/">Rockets</Link></li>
-          <li className="listItem"><Link className="listLink2" to="/Pages/Missions">Missions</Link></li>
-          <div className="vert-div" />
-          <li className="listItem"><Link className="listLink3" to="/Pages/MyProfilepage">My profile</Link></li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Rockets />} />
-        <Route path="/Pages/Missions" element={<Missions />} />
-        <Route path="/Pages/MyProfilepage" element={<MyProfile />} />
-      </Routes>
-    </div>
-  );
-}
+    <Routes>
+      <Route path="/" element={<Rockets />} />
+      <Route path="/Pages/Missions" element={<Missions />} />
+      <Route path="/Pages/MyProfilepage" element={<MyProfile />} />
+    </Routes>
+  </div>
+);
 
 export default App;
--- src/Pages/Rockets.js
+++ src/Pages/Rockets.js
@@ -1,7 +1,19 @@
+import { useEffect } from 'react';
+import { getApiRockets } from '../redux/rockets/Rocket';
+import store from '../redux/configureStore';
 import RocketsList from '../Components/MyRockets/RocketsList';
 
-const Rockets = () => (
-  <RocketsList />
-);
+let flag = false;
+const Rockets = () => {
+  useEffect(() => {
+    if (!flag) {
+      store.dispatch(getApiRockets());
+      flag = true;
+    }
+  });
+  return (
+    <RocketsList />
+  );
+};
 
 export default Rockets;
