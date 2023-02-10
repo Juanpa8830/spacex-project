f509785eebbdb9e3693b8cdbfedae5c7f945a5a6
Juanpa8830 <jp3_4@hotmail.com>
2023-02-10 12:14:10 -0500

    Refactor file with new variables

--- src/Components/MyProfileFolder/MyProfile.js
+++ src/Components/MyProfileFolder/MyProfile.js
@@ -6,17 +6,19 @@ import ProfileMissionItem from './ProfileMissionItem';
 const MyProfile = () => {
   const rockets = useSelector((state) => state.Rockets);
   const missions = useSelector((state) => state.Missions);
+  const joinedMissions = missions.filter((item) => item.joined === true);
+  const reservedRockets = rockets.filter((item) => item.reserved === true);
+
   return (
     <div className="container">
       <div className="profilediv" id="flex">
         <div className="joinedmissions">
-          <h1>My Missions</h1>
-
-          {missions.filter((item) => item.joined === true).length === 0 ? (
-            <h2>No Missions Joined</h2>)
+          <h2>My Missions</h2>
+          {joinedMissions.length === 0 ? (
+            <h3>No Missions Joined</h3>)
             : (
               <ul className="missionlist" id="flex">
-                {missions.filter((item) => item.joined === true).map((mission) => {
+                {joinedMissions.map((mission) => {
                   const {
                     name, id,
                   } = mission;
@@ -33,13 +35,12 @@ const MyProfile = () => {
 
         </div>
         <div className="resrockets">
-          <h1>My rockets</h1>
-
-          {rockets.filter((item) => item.reserved === true).length === 0 ? (
-            <h2>No rockets reserved</h2>)
+          <h2>My rockets</h2>
+          {reservedRockets.length === 0 ? (
+            <h3>No rockets reserved</h3>)
             : (
               <ul className="rocketlisted" id="flex">
-                {rockets.filter((item) => item.reserved === true).map((rocket) => {
+                {reservedRockets.map((rocket) => {
                   const {
                     name, id,
                   } = rocket;
